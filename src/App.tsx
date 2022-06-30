34d2b4ae425c295f8bfeed8c8c0c9a3cc3d85fc7
Jave Yambao <yambaolaysonjave@gmail.com>
2022-06-30 11:17:41 +0800

    added first half of todoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import InputField from './components/InputField';
+import {Todo} from "../model/models"
+import TodoList from './components/TodoList';
 
-function App() {
+// let name:string;
+// let age: number | string;
+// let isStudent: boolean;
+// let hobbies:string[];
+// let role:[number,string];
+
+// let printName: (name:string) => never;
+
+// type Person = {
+//   name: string
+//   age?: number
+// };
+
+// let person: Person = {
+//   name: 'jave'
+
+// };
+
+// let lotsOfPeople: Person[];
+
+// let personNAme: unknown;
+
+// interface Person {
+//   name: string;
+//   age: number;
+// }
+
+// interface Guy extends Person {
+//   profession: string;
+// }
+
+// type X = {
+//   a: string;
+//   b: number;
+// }
+
+// type Y = X & {
+//   c: string;
+//   d: number;
+// }
+
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo){
+      setTodos([...todos,{id: Date.now(), todo, isDone:false}]);
+      setTodo("");
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
